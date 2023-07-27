62849b765aa4c04705c694b5fbcff20923048b1f
nwjbrandon <nwjbrandon@gmail.com>
2023-07-27 14:18:31 +0800

    get average friends by age

--- data.py
+++ data.py
@@ -17,3 +17,10 @@ def load_ml_100k_data():
     lines = sc.textFile("data/ml-100k/u.data")
     rdd = lines.map(lambda x: x.split())
     return rdd
+
+
+def load_fakefriends():
+    # user id | name | age | number of friends
+    lines = sc.textFile("data/fakefriends.csv")
+    rdd = lines.map(lambda x: x.split(","))
+    return rdd
---
+++ main_fakefriends.py
@@ -0,0 +1,29 @@
+import collections
+
+from data import load_fakefriends, print_rdd
+
+
+def count_average_friends_by_age(rdd):
+    print("Count average friends by age")
+    rdd_ = rdd.map(lambda x: (int(x[2]), int(x[3])))
+    totals_by_age = rdd_.mapValues(lambda x: (x, 1)).reduceByKey(
+        lambda x, y: (x[0] + y[0], x[1] + y[1])
+    )
+    avg_by_age = totals_by_age.mapValues(lambda x: x[0] / x[1])
+    avg_by_age = collections.OrderedDict(sorted(avg_by_age.collect()))
+    for key, value in avg_by_age.items():
+        print("Age: %s | Friends: %i" % (key, value))
+
+
+def main():
+    rdd = load_fakefriends()
+    print_rdd(rdd)
+    print()
+
+    # Get average friends by age
+    count_average_friends_by_age(rdd)
+    print()
+
+
+if __name__ == "__main__":
+    main()
