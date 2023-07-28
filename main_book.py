40dd981d6187f061a6a4429def72eb4ea3aa0fd8
nwjbrandon <nwjbrandon@gmail.com>
2023-07-28 18:54:54 +0800

    count words after preprocessing

--- main_book.py
+++ main_book.py
@@ -1,5 +1,5 @@
 import collections
-
+import re
 from data import load_book_data, print_rdd
 
 
@@ -12,6 +12,16 @@ def count_word_occurences(rdd):
         key = key.encode("ascii", "ignore")
         print("Word: %s | Occurences: %i" % (key, value))
 
+def count_word_occurences_after_preprocessing(rdd):
+    print("Get word occurences after preprocessing")
+    rdd_ = rdd.flatMap(lambda x: re.compile(r'\W+', re.UNICODE).split(x.lower()))
+    word_counts = rdd_.countByValue()
+    word_counts = collections.OrderedDict(sorted(word_counts.items()))
+    for key, value in word_counts.items():
+        key = key.encode("ascii", "ignore")
+        print("Word: %s | Occurences: %i" % (key, value))
+
+
 
 def main():
     rdd = load_book_data()
@@ -22,6 +32,9 @@ def main():
     count_word_occurences(rdd)
     print()
 
+    # Get word occurences after preprocessing
+    count_word_occurences_after_preprocessing(rdd)
+    print()
 
 if __name__ == "__main__":
     main()
