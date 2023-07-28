a534666b5e30ab887246c1d4ef3c5e18236e0ec5
nwjbrandon <nwjbrandon@gmail.com>
2023-07-28 19:07:42 +0800

    get word occurences and sort by occurences

--- main_book.py
+++ main_book.py
@@ -8,7 +8,7 @@ def count_word_occurences(rdd):
     rdd_ = rdd.flatMap(lambda x: x.split())
     word_counts = rdd_.countByValue()
     word_counts = collections.OrderedDict(sorted(word_counts.items()))
-    for key, value in word_counts.items():
+    for key, value in list(word_counts.items())[:10]:
         key = key.encode("ascii", "ignore")
         print("Word: %s | Occurences: %i" % (key, value))
 
@@ -17,10 +17,18 @@ def count_word_occurences_after_preprocessing(rdd):
     rdd_ = rdd.flatMap(lambda x: re.compile(r'\W+', re.UNICODE).split(x.lower()))
     word_counts = rdd_.countByValue()
     word_counts = collections.OrderedDict(sorted(word_counts.items()))
-    for key, value in word_counts.items():
+    for key, value in list(word_counts.items())[:10]:
         key = key.encode("ascii", "ignore")
         print("Word: %s | Occurences: %i" % (key, value))
 
+def count_word_occurences_after_preprocessing_and_sorting(rdd):
+    print("Get sorted word occurences after preprocessing")
+    rdd_ = rdd.flatMap(lambda x: re.compile(r'\W+', re.UNICODE).split(x.lower()))
+    word_counts = rdd_.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y)
+    word_counts = word_counts.map(lambda x: (x[1], x[0])).sortByKey().map(lambda x: (x[1], x[0]))
+    for key, value in list(word_counts.collect())[:10]:
+        key = key.encode("ascii", "ignore")
+        print("Word: %s | Occurences: %i" % (key, value))
 
 
 def main():
@@ -34,6 +42,10 @@ def main():
 
     # Get word occurences after preprocessing
     count_word_occurences_after_preprocessing(rdd)
+    print()
+
+    # Get word occurences after preprocessing and sorting
+    count_word_occurences_after_preprocessing_and_sorting(rdd)
     print()
 
 if __name__ == "__main__":
